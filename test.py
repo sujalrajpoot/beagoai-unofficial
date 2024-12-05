c72ac006fdc654db3f55c477cd88d4419bbdfaec
Sujal Rajpoot <104921805+sujalrajpoot@users.noreply.github.com>
2024-12-05 17:46:01 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ from beagoai import BeagoAI_Unofficial
 
 if __name__ == "__main__":
     beagoai = BeagoAI_Unofficial()
-    print(beagoai.chat("what are the today's current affairs from defence direct education website"))
+    print(beagoai.chat("what are the today's current affairs from indian express website"))
